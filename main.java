9ef3100c39849506a9b251ed0e89e208224da447
Syed Safi <syedisaf@gmail.com>
2024-12-31 23:52:53 +0530

    second

--- main.java
+++ main.java
@@ -1,5 +1,19 @@
-public class main{
+public class main {
     public static void main(String[] args) {
         System.out.println("Welcome to the code practice ");
     }
 }
+
+// …or create a new repository on the command line
+// echo "# Practice-3.0" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/syedgithub12345/Practice-3.0.git
+// git push -u origin main
+
+// …or push an existing repository from the command line
+// git remote add origin https://github.com/syedgithub12345/Practice-3.0.git
+// git branch -M main
+// git push -u origin main
