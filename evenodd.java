6c9683fc433b7c7bbc640642d22376485049af09
Syed Safi <syedisaf@gmail.com>
2025-07-16 23:51:12 +0530

    seventy six

---
+++ evenodd.java
@@ -0,0 +1,11 @@
+public class evenodd {
+    public static void main(String[] args) {
+        int number = 10;
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+    }
+}
